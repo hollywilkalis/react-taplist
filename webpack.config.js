1a8c14b99fa811ca714363407db9a380ad66c7e3
Holly Wilkalis <holly.wilkalis@gmail.com>
2018-02-09 10:51:08 -0800

    Update Lint settings

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,18 @@ module.exports = {
   },
 
   module: {
-    rules: [
+    rrules: [
       {
+        test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: "./.eslintrc.json"
+          }
+        },
+        {
         test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
